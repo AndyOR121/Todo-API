189233d9a8e85533bae1bfe8aff49eb7185469de
Andrew O'Riordan <andrew.oriordan.tqs@gmail.com>
2016-03-23 13:19:54 +0000

    Added authentication token

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 var bcrypt = require('bcrypt');
 var _ = require('underscore');
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports = function(sequelize, DataTypes) {
 	var user = sequelize.define('user', {
@@ -55,7 +57,6 @@ module.exports = function(sequelize, DataTypes) {
 						if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
 							return reject();
 						}
-
 						resolve(user);
 					}, function(e) {
 						reject();
@@ -67,6 +68,27 @@ module.exports = function(sequelize, DataTypes) {
 			toPublicJSON: function() {
 				var json = this.toJSON();
 				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+			},
+			generateToken: function(type) {
+				if (!_.isString(type)) {
+					console.log('Not string');
+					return undefined;
+				}
+
+				try {
+					var stringData = JSON.stringify({
+						id: this.get('id'),
+						type: type
+					});
+					var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123').toString();
+					var token = jwt.sign({
+						token: encryptedData,
+					}, 'qwerty098');
+
+					return token;
+				} catch (e) {
+					return undefined;
+				}
 			}
 		}
 	});
